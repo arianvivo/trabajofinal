69e38f9732139b3fae8b0195676c9951905aaef7
arianvivo <arian.v.alesso@gmail.com>
2024-12-09 23:13:50 -0300

    fix formulario contacto

--- js/contacto.js
+++ js/contacto.js
@@ -7,13 +7,50 @@ const cleanFields = () => {
     document.getElementById("inputMessage").value = ""
 }
 
+const cleanWarnings = () => {
+    document.getElementById("name-warning").innerHTML = ""
+    document.getElementById("last-name-warning").innerHTML = ""
+    document.getElementById("email-warning").innerHTML = ""
+    document.getElementById("phone-warning").innerHTML = ""
+    document.getElementById("message-warning").innerHTML = ""
+}
+
+const checkFields = () => {
+    passChecks = true
+    if(document.getElementById("inputName").value == ""){
+        document.getElementById("name-warning").innerHTML = "Nombre inválido"
+        passChecks = false
+    }
+    if(document.getElementById("inputLastName").value == ""){
+        document.getElementById("last-name-warning").innerHTML = "Apellido inválido"
+        passChecks = false
+    }
+    if (!/^(\w|\d|-|_|\.)+@(\w|\d)+\.(\w){2,4}$/.test(document.getElementById("inputEmail").value)) {
+        document.getElementById("email-warning").innerHTML = "Mail inválido"
+        passChecks = false
+    }
+    if(!/^(\d){10,}/.test(document.getElementById("inputPhone").value)){
+        document.getElementById("phone-warning").innerHTML = "Teléfono inváido (al menos 10 dígitos)"
+        passChecks = false
+    }
+    if (document.getElementById("inputMessage").value.length < 10 ) {
+        document.getElementById("message-warning").innerHTML = "Debe escribir al menos 10 caracteres"
+        passChecks =false
+    }
+
+    return passChecks
+    
+}
 let sendForm = document.getElementById("enviar-formulario")
 
 sendForm.addEventListener("click", (e) => {
     e.preventDefault()
-    cleanFields()
-    Swal.fire({
-        text: "Enviado!",
-        icon: "success"
-      });
+    cleanWarnings()
+    if (checkFields()){
+        Swal.fire({
+            text: "Enviado!",
+            icon: "success"
+        });
+        cleanFields()
+    }
 })
